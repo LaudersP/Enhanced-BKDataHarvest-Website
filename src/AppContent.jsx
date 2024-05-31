6f31037fb654230317ca3617e9728fc6cd15635e
Parker Lauders <parkerlauders04@gmail.com>
2024-05-30 20:55:46 -0400

    Added routing for the menu page

--- src/AppContent.jsx
+++ src/AppContent.jsx
@@ -3,6 +3,7 @@ import Header from "./components/display/Header/Header";
 import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Footer from "./components/display/Footer/Footer";
+import MenuPage from "./pages/MenuPage/MenuPage";
 
 function AppContent({ initialData }) {
   const location = useLocation();
@@ -12,6 +13,7 @@ function AppContent({ initialData }) {
       <Header path={location.pathname} data={initialData} />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
       </Routes>
       <Footer />
     </>
