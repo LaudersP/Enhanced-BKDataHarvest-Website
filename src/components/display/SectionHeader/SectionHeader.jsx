1177fec7f57611bf8811ed7c4fe3141dc2296759
Parker Lauders <parkerlauders04@gmail.com>
2024-05-31 13:52:59 -0400

    Fixed a bug that prevented the border top from being toggled off

--- src/components/display/SectionHeader/SectionHeader.jsx
+++ src/components/display/SectionHeader/SectionHeader.jsx
@@ -3,7 +3,9 @@ import "./SectionHeader.css";
 
 function SectionHeader({ removeTop = false, label }) {
   return (
-    <div className={`section-header ${removeTop ? "no-top-border" : ""}`}>
+    <div
+      className={`section-header ${removeTop ? "no-top-border" : "border-top"}`}
+    >
       <h3 className="label">{label}</h3>
     </div>
   );
