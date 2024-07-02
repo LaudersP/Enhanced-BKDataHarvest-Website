c11ccda08974b3b931ac5213ddc6e24355ceffe1
Parker Lauders <parkerlauders04@gmail.com>
2024-07-02 01:56:33 -0400

    Rewrote the footer

--- src/components/display/Footer/Footer.jsx
+++ src/components/display/Footer/Footer.jsx
@@ -8,16 +8,6 @@ function Footer() {
         This website is a personal portfolio project and is not affiliated with,
         endorsed by, or sponsored by Burger King Corporation.
       </p>
-      <a
-        href="https://www.bk.com/trademarks"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Burger King Corporation Trademarks,
-      </a>
-      <p className="footer-text">
-        and others, are used here for informational purposes only.
-      </p>
     </footer>
   );
 }
